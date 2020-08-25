d4dd5fcd0cd952288e6854e8e01196f410975dbe
albertxu <17albertX@students.harker.org>
2020-08-25 14:39:43 -0400

    use async

--- main.js
+++ main.js
@@ -2,13 +2,13 @@ const fs = require('fs');
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
+client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`);
   const ch = await client.channels.fetch('733207005304586272');
   ch?.send('Got an update! Reloading bot.');
 });
 
-client.on('message', msg => {
+client.on('message', async msg => {
   if (msg.content == 'ping') {
     console.log(`Message from ${msg.author.username}`);
     msg.channel.send('pong');
