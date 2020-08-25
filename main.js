bd6c072e5fa89924f20e480436c86f235e56a5d3
albertxu <17albertX@students.harker.org>
2020-08-24 23:55:10 -0400

    Read bot token properly now

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-console.log('Hello World!');
-
+const fs = require('fs');
 const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+const tok = fs.readFileSync('token.txt');
+console.log(tok.toString());
+
+console.log('done.');
