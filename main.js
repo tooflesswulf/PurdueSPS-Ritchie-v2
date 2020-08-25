e5ef962cc87909cf725edb4953facc664dc68906
albertxu <17albertX@students.harker.org>
2020-08-25 14:49:23 -0400

    Remove extraneous log statements

--- main.js
+++ main.js
@@ -5,16 +5,12 @@ const client = new Discord.Client();
 client.on('ready', async () => {
   console.log(`Logged in as ${client.user.tag}!`);
 
-  console.log('Wow i cant fetch a channel');
-
   client.channels.fetch('733467857719001158').then(ch => {
     console.log(`ch id: ${ch.id}`);
     ch.send('Got an update! Reloading bot.');
   }).catch(err => {
     console.log(err);
   });
-
-  console.log('hello?');
 });
 
 client.on('message', async msg => {
