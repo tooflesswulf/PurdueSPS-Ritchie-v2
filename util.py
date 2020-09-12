3df8c7d81304c45431b2a0863d034f03dd4fc0bf
albertxu <17albertX@students.harker.org>
2020-09-12 00:47:49 -0400

    util fix

--- util.py
+++ util.py
@@ -1,10 +1,10 @@
 from typing import List
 
 # Formats a bunch of lines into a list of lines<2000 chars
-def long_print(lines: str) -> List[str]:
+def long_print(lines: List[str]) -> List[str]:
     s = ''
     out = []
-    for l in lines.split('\n'):
+    for l in lines:
         if len(s) + len(l) + 1 < 2000:
             s += l + '\n'
         else:
