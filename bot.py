1622386b096403371bd4128525f9fe2af9f5039f
albertxu <17albertX@students.harker.org>
2020-09-11 23:56:07 -0400

    print log lulw

--- bot.py
+++ bot.py
@@ -18,6 +18,23 @@ async def ping(ctx: discord.ext.commands.Context):
     await ctx.send('pong2')
 
 
+@client.command()
+async def log(ctx: discord.ext.commands.Context):
+    if ctx.guild.id != 286028084287635456:
+        return
+
+    with open('log.txt', 'r') as f:
+        s = ''
+        for l in f.readlines():
+            if len(s) + len(l) + 1 < 2000:
+                s += l + '\n'
+            else:
+                ctx.send(s)
+                s = l + '\n'
+        if len(s) > 0:
+            ctx.send(s)
+
+
 with open('token.txt', 'r') as f:
     tok = f.readline()
     client.run(tok)
