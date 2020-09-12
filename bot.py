83a399de0df8aa804e03b6723795427113d0c5d9
albertxu <17albertX@students.harker.org>
2020-09-11 23:30:47 -0400

    run w/ token

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ client = commands.Bot(command_prefix='!')
 @client.event
 async def on_ready():
     print('Logged in as {}!'.format(client.user))
-    
+
     hello_ch = client.get_channel(754173414867992686)
     if hello_ch is not None:
         await hello_ch.send('(Re)starting bot!')
@@ -16,3 +16,8 @@ async def on_ready():
 @client.command()
 async def ping(ctx: discord.ext.commands.Context):
     await ctx.send('pong2')
+
+
+with open('token.txt', 'r') as f:
+    tok = f.readline()
+    client.run(tok)
